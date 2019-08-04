81cfdd814cd8b9b7519b829fa3e54d3d31e0d837
RongDai430 <dairong430@gmail.com>
2019-08-04 14:16:15 +0800

    Update SortTestHelper.h

--- Algorithms & Data Structures/Ch2-Sorting-Basic/SortTestHelper.h
+++ Algorithms & Data Structures/Ch2-Sorting-Basic/SortTestHelper.h
@@ -8,7 +8,7 @@
 
 #include<iostream>
 #include<ctime>
-#include<cassert> 
+#include<cassert>
 
 using namespace std; 
 
@@ -33,6 +33,38 @@ namespace SortTestHelper{
 		cout<<endl;
 		return;
 		}
+	
+	
+	//是否排序的判断函数，模板函数
+	template<typename T>
+	bool isSorted(T arr[], int n){
+		
+		for(int i = 0; i < n - 1; i++)
+			if(arr[i] > arr[i + 1]) return false;
+		
+		return true;
+	} 
+	
+	//测试排序函数时间，模板函数，传入sort函数 
+	template<typename T>
+	void testSort(string sortName, void(*sort)(T[], int), T arr[], int n){
+		
+		clock_t startTime = clock();
+		sort(arr, n);
+		clock_t endTime = clock();
+		
+		assert(isSorted(arr,n));
+		cout << sortName << ":" << double(endTime - startTime) / CLOCKS_PER_SEC << "s" << endl;
+		
+		return;
+	}
+	
+	//复制数组 
+	int* copyIntArrary(int a[],  int n){
+		int *arr = new int[n];
+		copy(a, a + n, arr);
+		return arr;
 	}
 
+}
 #endif
